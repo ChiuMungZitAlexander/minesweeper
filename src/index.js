baee6d559dbf1a4b057a35a1063bac23f7f1a1f8
AlexanderZhao <zhaomengzhe@qq.com>
2018-05-14 18:26:10 +0800

    refactor page

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React, { Component } from 'react'
-import Comp from './components'
+import React from 'react'
 
-export default class Main extends Component {
-	render () {
-		return (
-			<main>
-				<section className="panel-area">
-					<div className="counter">counter</div>
-					<div className="timer">timer</div>
-				</section>
-				<section className="play-are"></section>
-			</main>
-		)
+import MineBoard from './components/mine-board'
+import { DIFFICULTY } from './const'
+
+ class Main extends React.Component {
+		state = {
+			difficulty: DIFFICULTY.EASY
+		}
+
+		render () {
+			return (
+				<main>
+					<section className="panel-area">
+						<div className="counter">counter</div>
+						<div className="timer">timer</div>
+					</section>
+					<section className="play-area">
+						<MineBoard difficulty={this.state.difficulty} />
+					</section>
+				</main>
+			)
+		}
 	}
-}
+
+export default Main
