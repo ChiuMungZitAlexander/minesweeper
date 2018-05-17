2a732697b49442f271433c89db0d8d7f1e16dd8f
AlexanderZhao <zhaomengzhe@qq.com>
2018-05-17 14:11:16 +0800

    add css-loader and style-loader

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import Main from './src/index'
+import 'style.css'
 
 const myElement = <Main />;
 render(myElement, document.body);
