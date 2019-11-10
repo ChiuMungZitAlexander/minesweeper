432886f7380b7d1e1441132cabff3a6dc5f7e266
bee0060 <be0060@126.com>
2019-11-10 23:11:37 +0800

    update workflow to use 3rd part actions

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
 		extensions: ['.js']
 	},
 	output: {
-		path: path.resolve(__dirname, './'),
+		path: path.resolve(__dirname, 'dist'),
 		filename: 'minesweeper.bundle.js',
 	},
 	module: {
